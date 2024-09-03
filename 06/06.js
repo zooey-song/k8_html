60765ae89393b5bc9e4038df0d23314ac639c65d
minimonimo <minimonimo@gmail.com>
2024-09-03 16:52:45 +0900

    20240903 주사위갬블

--- 06/06.js
+++ 06/06.js
@@ -1,13 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const bt1 = document.querySelector('button');
-  const img = document.querySelector('#msg > img');
+document.addEventListener('DOMContentLoaded', ()=>{
+  //이미지 가져오기
+  const imgs = document.querySelectorAll('.dice > img') ;
+  //버튼 가져오기
+  const bts = document.querySelectorAll('button') ;
+  //결과 가져오기 
+  const msg = document.querySelector('#msg') ;
 
-  bt1.addEventListener('click', ()=>{
-    let n = Math.floor(Math.random()*6)+1;
-    img.setAttribute('src',`../img/${n}.png`);
-    //alert(n);
-    img.setAttribute('alt',`${n}`);
+  //console.log(bts);
 
+  //버튼 클릭
+  bts[0].addEventListener('click', ()=>{
+    
   });
+   for(let bt of bts){
+     bt.addEventListener('click', ()=>{
+       let comN = Math.floor(Math.random() * 6)+1;
+       let userN = parseInt(bt.textContent.charAt(0));
+       //console.log(bt.textContent.charAt(0));
+       
 
+      imgs[0].setAttribute('src',`../img/${comN}.png`);
+      imgs[0].setAttribute('alt','computer num ${comN}');
+
+      imgs[1].setAttribute('src',`../img/${userN}.png`);
+      imgs[1].setAttribute('alt','user num ${userN}');
+
+       if(comN === userN) msg.innerHTML = '맞음';
+       else msg.innerHTML = '틀림';
+
+     });
+   };
 });
